6749937fc535c23d924a9a616638209771601d32
owen20108 <owen20108@yahoo.com>
2023-11-20 15:26:13 -0800

    sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,9 +1,8 @@
 #include "sort.h"
 
 /**
- * cocktail_sort_list - sort a dlined list
- * @list: list
- * Return: none
+ * cocktail_sort_list - Sorts a doubly linked list using the Cocktail Shaker sort algorithm.
+ * @list: Pointer to the head of the linked list (not used in this implementation).
  */
 void cocktail_sort_list(listint_t **list)
 {
