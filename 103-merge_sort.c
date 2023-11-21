23fecde461db2fed3248061d6852738d0260151a
owen20108 <owen20108@yahoo.com>
2023-11-20 16:08:57 -0800

    sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -8,20 +8,18 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    unsigned int i = 0;
-    char *p;
+	unsigned int i = 0;
+	char *p;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = '\0';
 
-    p = malloc(nmemb * size);
-    if (p == NULL)
-        return (NULL);
-
-    for (i = 0; i < (nmemb * size); i++)
-        p[i] = '\0';
-
-    return (p);
+	return (p);
 }
 /**
  *merge - make a merge
